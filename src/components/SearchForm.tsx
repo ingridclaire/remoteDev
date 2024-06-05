088ff401bcc22403417406f300e74d25c06107a2
Ingrid Carlson <ingridclaire@gmail.com>
2024-06-05 12:24:08 -0400

    initial project set up, components added

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export default function SearchForm() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <form action="#" className="search">
+    <form onSubmit={handleSubmit} action="#" className="search">
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchTerm}
+        onChange={handleChange}
         spellCheck="false"
         type="text"
         required
